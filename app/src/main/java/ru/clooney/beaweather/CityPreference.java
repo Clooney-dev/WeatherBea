433d2f28f2c91eb0d03df81cc322c08196aeb715
Clooney-dev <561754@mail.ru>
2021-06-01 18:58:58 +0300

    Test

---
+++ app/src/main/java/ru/clooney/beaweather/CityPreference.java
@@ -0,0 +1,24 @@
+package ru.clooney.beaweather;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+public class CityPreference {
+
+    SharedPreferences prefs;
+
+    public CityPreference(Activity activity){
+        prefs = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+
+    // If the user has not chosen a city yet, return
+    // Voronezh as the default city
+    public String getCity(){
+        return prefs.getString("city", "Voronezh, RU");
+    }
+
+    void setCity(String city){
+        prefs.edit().putString("city", city).commit();
+    }
+
+}
